1e4b447d3d184629a8949c155a5781131fe7b11a
권유리 <55318618+YuriKwon@users.noreply.github.com>
2023-04-26 07:48:35 +0000

    feat: post create 쉘 스크립트 추가

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,34 @@
+# 임의로 실행시키기 위해 필요한 쉘 스크립트
+
+## Install additional apt packages
+sudo apt-get update \
+    && sudo apt-get install -y dos2unix libsecret-1-0 xdg-utils \
+    && sudo apt clean -y \
+    && sudo rm -rf /var/lib/apt/lists/*
+
+## Configure git
+git config --global pull.rebase false
+git config --global core.autocrlf input
+
+## GitHub Copilot CLI ##
+npm install -g @githubnext/github-copilot-cli
+eval "$(github-copilot-cli alias -- "$0")"
+
+## GitHub Copilot CLI ##
+# 코파일럿은 터미널에서도 돈다!
+echo '
+# Add GitHub Copilot CLI alias
+alias ghcp='github-copilot-cli'
+eval "$(github-copilot-cli alias -- "$0")"
+' >> $HOME/.zshrc
+echo '
+# Add GitHub Copilot CLI alias
+alias ghcp='github-copilot-cli'
+eval "$(github-copilot-cli alias -- "$0")"
+' >> $HOME/.bashrc
+
+## AZURE BICEP CLI ##
+az bicep install
+
+## AZURE DEV CLI ##
+curl -fsSL https://aka.ms/install-azd.sh | bash
